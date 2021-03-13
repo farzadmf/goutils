a5aeb47bde9553c8d5127520b7fb711e013b7c42
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2021-03-13 09:13:47 -0500

    Add comments for functions

--- commands.go
+++ commands.go
@@ -9,7 +9,9 @@ import (
 	"syscall"
 )
 
-// SpawnCommand runs a command and shows its output
+// SpawnCommand runs a command and shows its output. It's going to start the command and give
+//      control to that process, so if we, say, do Ctrl+C, we're basically existing from the whole
+//      application and we won't return to our original process
 func SpawnCommand(workingDir, cmdStr string) error {
 	cmd := exec.Command("bash", "-c", cmdStr)
 	cmd.Dir = workingDir
@@ -34,7 +36,7 @@ func ExecCommand(workingDir, cmd string) error {
 	return syscall.Exec(bash, []string{"bash", "-c", cmd}, os.Environ())
 }
 
-// RunCommand runs a shell command
+// RunCommand runs a shell command as a subprocess and returns its output
 func RunCommand(cmdString string) (string, error) {
 	cmd := exec.Command("bash", "-c", cmdString)
 
@@ -46,7 +48,9 @@ func RunCommand(cmdString string) (string, error) {
 	return string(combined), nil
 }
 
-// StartCommand uses StartProcess to start a command
+// StartCommand uses StartProcess to start a command. It's going to start the command, but the
+//      "control" is still in the calling process, so when the command finishes, we return back
+//      to our original process
 func StartCommand(workingDir, cmdStr string) error {
 	bash, err := exec.LookPath("bash")
 	if err != nil {
