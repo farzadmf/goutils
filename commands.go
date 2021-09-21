068b802bf801db0efdf3d52a026453585f8d3eda
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2021-09-21 09:24:51 -0400

    Fix comments in commands.go and change spawn to allow hiding the output

--- commands.go
+++ commands.go
@@ -9,20 +9,24 @@ import (
 	"syscall"
 )
 
-// SpawnCommand runs a command and shows its output. It's going to start the command and give
-//      control to that process, so if we, say, do Ctrl+C, we're basically existing from the whole
-//      application and we won't return to our original process
-func SpawnCommand(workingDir, cmdStr string) error {
+// SpawnCommand runs a command. It will show the output if 'quiet' is true, otherwise,
+// it won't show anything.
+func SpawnCommand(workingDir, cmdStr string, quiet bool) error {
 	cmd := exec.Command("bash", "-c", cmdStr)
 	cmd.Dir = workingDir
-	var stdout, stderr bytes.Buffer
-	cmd.Stdout = io.MultiWriter(os.Stdout, &stdout)
-	cmd.Stderr = io.MultiWriter(os.Stderr, &stderr)
+
+	if !quiet {
+		var stdout, stderr bytes.Buffer
+		cmd.Stdout = io.MultiWriter(os.Stdout, &stdout)
+		cmd.Stderr = io.MultiWriter(os.Stderr, &stderr)
+	}
 
 	return cmd.Run()
 }
 
-// ExecCommand runs a command and shows its output
+// ExecCommand runs a command and shows its output. It's going to start the command and give
+//      control to that process, so if we, say, do Ctrl+C, we're basically existing from the whole
+//      application and we won't return to our original process.
 func ExecCommand(workingDir, cmd string) error {
 	bash, err := exec.LookPath("bash")
 	if err != nil {
@@ -36,7 +40,7 @@ func ExecCommand(workingDir, cmd string) error {
 	return syscall.Exec(bash, []string{"bash", "-c", cmd}, os.Environ())
 }
 
-// RunCommand runs a shell command as a subprocess and returns its output
+// RunCommand runs a shell command as a subprocess and returns its output.
 func RunCommand(cmdString string) (string, error) {
 	cmd := exec.Command("bash", "-c", cmdString)
 
@@ -50,7 +54,7 @@ func RunCommand(cmdString string) (string, error) {
 
 // StartCommand uses StartProcess to start a command. It's going to start the command, but the
 //      "control" is still in the calling process, so when the command finishes, we return back
-//      to our original process
+//      to our original process.
 func StartCommand(workingDir, cmdStr string) error {
 	bash, err := exec.LookPath("bash")
 	if err != nil {
